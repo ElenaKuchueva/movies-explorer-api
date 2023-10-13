28ae25e9ebb7afa3728370990853a1273236f42e
ElenaKuchueva <120306299+ElenaKuchueva@users.noreply.github.com>
2023-10-14 00:24:20 +0300

    изменение порта на 4000

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const allRoutes = require('./routes/allRoutes');
 const centralErrorHandler = require('./middlewares/centralErrorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 
 const app = express();
 app.use(limiter);
